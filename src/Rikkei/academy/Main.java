b868c593a1df59570e4e72cc4e9bc513f163c6d0
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-03-07 16:53:09 +0700

    0703

--- src/Rikkei/academy/Main.java
+++ src/Rikkei/academy/Main.java
@@ -1,2 +1,112 @@
-package Rikkei.academy;public class Main {
+package Rikkei.academy;
+
+import java.time.Year;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        int size = 0;
+        Student[] studentsArray = new Student[200];
+        boolean formStatus = false;
+        do {
+            System.out.println("\n****************HACKATHON-JAVA-ADVANCE-1-MENU***************");
+            System.out.println("1. Nhập số sinh viên và nhập thông tin cho các sinh viên");
+            System.out.println("2. In thông tin các sinh viên đang quản lý");
+            System.out.println("3. Sắp xếp các sinh viên theo điểm trung bình tăng dần");
+            System.out.println("4. Nhập vào tên sinh viên và tìm kiếm sinh viên theo tên");
+            System.out.println("5. Thống kê số sinh viên nam, nữ đang quản lý");
+            System.out.println("6. In ra thông tin các sinh viên xếp loại giỏi và trung bình ");
+            System.out.println("7. Thoát");
+            System.out.println("Nhập lựa chọn của bạn: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Nhập số lượng sinh viên bạn muốn nhập: ");
+                    size = sc.nextInt();
+                    if (size > 0) {
+                        for (int i = 0; i < size; i++) {
+                            System.out.println("Nhập thông tin sinh viên thứ " + (i + 1) + " :");
+                            studentsArray[i] = new Student();
+                            studentsArray[i].inputStudent(size);
+                        }
+                        formStatus = true;
+                    } else {
+                        System.out.println("Không hợp lệ.");
+                    }
+                    break;
+                case 2:
+                    if (formStatus)
+                        for (int i = 0; i < size; i++) {
+                            studentsArray[i].displayData();
+                        }
+                    else
+                        System.out.println("Chưa có sinh viên nào.");
+                    break;
+                case 3:
+                    Student temp = studentsArray[0];
+                    if (formStatus)
+                        for (int i = 0; i < size - 1; i++) {
+                            for (int j = 1; j < size; j++) {
+                                if (studentsArray[i].calAvgMark() > studentsArray[j].calAvgMark()) {
+                                    temp = studentsArray[j];
+                                    studentsArray[j] = studentsArray[i];
+                                    studentsArray[i] = temp;
+                                }
+                            }
+                        }
+                    else {
+                        System.out.println("Chưa có sinh viên.");
+                    }
+                    break;
+                case 4:
+                    if (formStatus) {
+                        System.out.println("Nhập vào tên sinh viên bạn muốn tìm: ");
+                        sc.nextLine();
+                        String name = sc.nextLine();
+                        for (int i = 0; i < size; i++) {
+                            if (studentsArray[i].getStudentName().equalsIgnoreCase(name))
+                                studentsArray[i].displayData();
+                        }
+                    } else {
+                        System.out.println("Chưa có sinh viên.");
+                    }
+                    break;
+                case 5:
+                    int countMale = 0;
+                    int countFemale = 0;
+                    if (formStatus) {
+                        for (int i = 0; i < size; i++) {
+                            if (studentsArray[i].getSex() == 1)
+                                countMale++;
+                            else
+                                countFemale++;
+                        }
+                        System.out.println("Tổng số sinh viên nam: " + countMale + "\nTổng số sinh viên nữ: " + countFemale);
+                    } else {
+                        System.out.println("Chưa có sinh viên.");
+                    }
+                    break;
+                case 6:
+                    if (formStatus) {
+                        System.out.println("\nCác sinh viên xếp loại giỏi:");
+                        for (int i = 0; i < size; i++) {
+                            if (studentsArray[i].calAvgMark() >= 8 && studentsArray[i].calAvgMark() <9)
+                                studentsArray[i].displayData();
+                        }
+                        System.out.println("\n\nCác sinh viên xếp loại TB: ");
+                        for (int i = 0; i < size; i++) {
+                            if (studentsArray[i].calAvgMark() >= 5 && studentsArray[i].calAvgMark() < 7)
+                                studentsArray[i].displayData();
+                        }
+                    } else {
+                        System.out.println("Chưa có sinh viên.");
+                    }
+                    break;
+                case 7:
+                    System.exit(0);
+            }
+        } while (choice != 7);
+    }
 }
--- src/Rikkei/academy/Student.java
+++ src/Rikkei/academy/Student.java
@@ -1,2 +1,154 @@
-package Rikkei.academy;public class Student {
+package Rikkei.academy;
+
+import java.time.Year;
+import java.util.Scanner;
+
+public class Student {
+    private String studentIdNumber, studentId, studentName, phone, address;
+
+    private final String studentIdCode = "RA";
+    private int age, sex;
+    float html, css, javascript, avgMark;
+
+    public Student() {
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentIdNumber() {
+        return studentIdNumber;
+    }
+
+    public void setStudentIdNumber(String studentIdNumber) {
+        this.studentIdNumber = studentIdNumber;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getHtml() {
+        return html;
+    }
+
+    public void setHtml(float html) {
+        this.html = html;
+    }
+
+    public float getCss() {
+        return css;
+    }
+
+    public void setCss(float css) {
+        this.css = css;
+    }
+
+    public float getJavascript() {
+        return javascript;
+    }
+
+    public void setJavascript(float javascript) {
+        this.javascript = javascript;
+    }
+
+    public float getAvgMark() {
+        return avgMark;
+    }
+
+    public void setAvgMark(float avgMark) {
+        this.avgMark = avgMark;
+    }
+
+    public void displayData() {
+        int today = Year.now().getValue();
+        System.out.println("\nThông tin:");
+        System.out.printf("%s%-20s%s%s", "Mã sinh viên: ", mergeStudentId(), "Tên sinh viên: ", studentName);
+        System.out.printf("%s%-24d%s%-20s%s%s", "\nNăm sinh: ", (today - age), "Giới tính: ", (sex == 1 ? "Nam" : sex == 2 ? "Nữ" : "Khác"), "Điện thoại: ", phone);
+        System.out.printf("%s%-56s%s%f", "\nĐịa chỉ: ", address, "Điểm TB: ", calAvgMark());
+    }
+
+    public void inputStudent(int size) {
+        Scanner sc = new Scanner(System.in);
+        int inputSex = 0;
+        String inputId = "";
+        do {
+            System.out.println("Nhập mã sinh viên (3 chữ số):");
+            inputId = sc.nextLine();
+            if (inputId.length() != 3)
+                System.out.println("ID chưa đúng yêu cầu");
+        } while (inputId.length() != 3);
+        this.setStudentIdNumber(inputId);
+        System.out.println("Nhập tên sinh viên: ");
+        this.setStudentName(sc.nextLine());
+        System.out.println("Nhập tuổi: ");
+        this.setAge(sc.nextInt());
+        do {
+            System.out.println("Nhập giới tính: (Nam = 1; Nữ = 2; Khác = 3)");
+            inputSex = sc.nextInt();
+            if (inputSex < 1 || inputSex > 3)
+                System.out.println("Giới tính không hợp lệ.");
+        } while (inputSex < 1 || inputSex > 3);
+        this.setSex(inputSex);
+        sc.nextLine();
+        System.out.println("Nhập số điện thoại: ");
+        this.setPhone(sc.nextLine());
+        System.out.println("Nhập địa chỉ: ");
+        this.setAddress(sc.nextLine());
+        System.out.println("Nhập điểm HTML: ");
+        this.setHtml(sc.nextFloat());
+        System.out.println("Nhập điểm CSS: ");
+        this.setCss(sc.nextFloat());
+        System.out.println("Nhập điểm JavaScript: ");
+        this.setJavascript(sc.nextFloat());
+    }
+
+    public double calAvgMark() {
+        return avgMark = (html + css + 2 * javascript) / 4;
+    }
+
+    private String mergeStudentId() {
+        return studentId = studentIdCode + studentIdNumber;
+    }
 }
